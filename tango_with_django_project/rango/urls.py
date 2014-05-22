5067c96b9478bb48f245c3a8121576baa2444755
Santu Mahapatra <santumahapatra@gmail.com>
2014-05-22 18:18:51 +0530

    Chapter 6: Adding Category views and URLs

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, url
 from rango import views
 
 urlpatterns = patterns('',
-  url(r'^$', views.index, name='index'))
+  url(r'^$', views.index, name='index'),
+  url(r'^category/(?P<category_name_url>\w+)/$', views.category, name='category'),
+  )
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
-from rango.models import Category
+from rango.models import Category, Page
 
 def index(request):
   context = RequestContext(request)
@@ -11,3 +11,20 @@ def index(request):
   category_list = Category.objects.order_by('-name')[:5]
   context_dict = {'categories': category_list}
   return render_to_response('rango/index.html', context_dict, context)
+
+def category(request, category_name_url):
+  context = RequestContext(request)
+
+  category_name = category_name_url.replace('_', ' ')
+
+  context_dict = {'category_name': category_name}
+
+  try:
+    category = Category.objects.get(name=category_name)
+    pages = Page.objects.filter(category=category)
+    context_dict['pages'] = pages
+    context_dict['category'] = category
+  except Category.DoesNotExist:
+    pass
+
+  return render_to_response('rango/category.html', context_dict, context)
