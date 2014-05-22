ff32f5e4867bf3d9fd07a5dfa9b63c2dedc72dfb
Santu Mahapatra <santumahapatra@gmail.com>
2014-05-22 22:55:03 +0530

    Chapter 8: The Registration Form

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
   url(r'^$', views.index, name='index'),
   url(r'^add_category/$', views.add_category, name='add_category'),
   url(r'^category/(?P<category_name_url>\w+)/$', views.category, name='category'),
+  url(r'^register/$', views.register, name='register'),
   )
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,7 +4,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 from rango.models import Category, Page
-from rango.forms import CategoryForm
+from rango.forms import CategoryForm, UserForm, UserProfileForm
 
 def index(request):
   context = RequestContext(request)
@@ -51,3 +51,38 @@ def add_category(request):
     form = CategoryForm()
 
   return render_to_response('rango/add_category.html', {'form': form}, context)
+
+def register(request):
+  context = RequestContext(request)
+  registered = False
+
+  if request.method == 'POST':
+    user_form = UserForm(data=request.POST)
+    profile_form = UserProfileForm(data=request.POST)
+
+    if user_form.is_valid() and profile_form.is_valid():
+      user = user_form.save()
+      user.set_password(user.password)
+      user.save()
+
+      profile = profile_form.save(commit=False)
+      profile.user = user
+
+      if 'picture' in request.FILES:
+        profile.picture = request.FILES['picture']
+
+      profile.save()
+
+      registered = True
+
+    else:
+      print user_form.errors, profile_form.errors
+
+  else:
+    user_form = UserForm()
+    profile_form = UserProfileForm()
+
+  return render_to_response(
+    'rango/register.html',
+    {'user_form':user_form, 'profile_form':profile_form, 'registered':registered},
+    context)
