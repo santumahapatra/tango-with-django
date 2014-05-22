6d8163aad5b7a5b3d0333df07571f03f925b1644
Santu Mahapatra <santumahapatra@gmail.com>
2014-05-22 19:17:21 +0530

    Chapter 7: Adding form to add categories

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,6 +4,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 def index(request):
   context = RequestContext(request)
@@ -32,3 +33,21 @@ def category(request, category_name_url):
     pass
 
   return render_to_response('rango/category.html', context_dict, context)
+
+def add_category(request):
+  context = RequestContext(request)
+
+  if request.method == 'POST':
+    form = CategoryForm(request.POST)
+
+    if form.is_valid():
+      form.save(commit=True)
+
+      return index(request)
+    else:
+      print form.errors
+
+  else:
+    form = CategoryForm()
+
+  return render_to_response('rango/add_category.html', {'form': form}, context)
