306b577394fea03ca709e93984827aa41ca00790
Santu Mahapatra <santumahapatra@gmail.com>
2014-06-10 13:06:57 +0530

    Storting session data in server, only session ID in client

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -27,22 +27,19 @@ def index(request):
   page_list = Page.objects.order_by('-views')[:5]
   context_dict['pages'] = page_list
 
-  response = render_to_response('rango/index.html', context_dict, context)
+  if request.session.get('last_visit'):
+    last_visit_time = request.session.get('last_visit')
+    visits = request.session.get('visits', 0)
 
-  visits = int(request.COOKIES.get('visits', '0'))
-
-  if 'last_visit' in request.COOKIES:
-    last_visit = request.COOKIES['last_visit']
-    last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
-
-    if (datetime.now() - last_visit_time).days > 0:
-      response.set_cookie('visits', visits+1)
-      response.set_cookie('last_visit', datetime.now())
+    if (datetime.now() - datetime.strptime(last_visit_time[:-7], "%Y-%m-%d %H:%M:%S")).days > 0:
+      request.session['visits'] = visits+1
+      request.session['last_visit'] = str(datetime.now())
 
   else:
-    response.set_cookie('last_visit', datetime.now())
+    request.session['last_visit'] = str(datetime.now())
+    request.session['visits'] = 1
 
-  return response
+  return render_to_response('rango/index.html', context_dict, context)
 
 def category(request, category_name_url):
   context = RequestContext(request)
